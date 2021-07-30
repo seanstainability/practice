61f311f4ed1fef3096b2e327deaeecd4fda64b03
seanstainability <seanstainability@seanstainabilityui-MacBookPro.local>
2021-07-30 21:38:42 +0900

    리팩토링: 이벤트 위임

--- todolist/main.js
+++ todolist/main.js
@@ -12,33 +12,42 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; // UUID
 function createElement(text) {
   const content = document.createElement("div");
   content.setAttribute("class", "content");
+  content.setAttribute("data-id", id);
+  content.innerHTML = `
+    <li class="list">
+      <span class="list_title">${text}</span
+      ><button class="delBtn"><i class="far fa-trash-alt" data-id=${id}></i></button>
+    </li>
+    <div class="divider"></div>
+  `;
 
-  const list = document.createElement("li");
-  list.setAttribute("class", "list");
+  // const list = document.createElement("li");
+  // list.setAttribute("class", "list");
 
-  const title = document.createElement("span");
-  title.setAttribute("class", "list_title");
-  title.innerText = text;
+  // const title = document.createElement("span");
+  // title.setAttribute("class", "list_title");
+  // title.innerText = text;
 
-  const delBtn = document.createElement("button");
-  delBtn.setAttribute("class", "delBtn");
-  delBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
-  delBtn.addEventListener("click", (e) => {
-    contents.removeChild(content);
-  });
+  // const delBtn = document.createElement("button");
+  // delBtn.setAttribute("class", "delBtn");
+  // delBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+  // delBtn.addEventListener("click", (e) => {
+  //   contents.removeChild(content);
+  // });
 
-  const divider = document.createElement("div");
-  divider.setAttribute("class", "divider");
+  // const divider = document.createElement("div");
+  // divider.setAttribute("class", "divider");
 
-  list.appendChild(title);
-  list.appendChild(delBtn);
+  // list.appendChild(title);
+  // list.appendChild(delBtn);
 
-  content.appendChild(list);
-  content.appendChild(divider);
-
+  // content.appendChild(list);
+  // content.appendChild(divider);
+  id++;
   return content;
 }
 
@@ -49,5 +58,14 @@ addBtn.addEventListener("click", (e) => {
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     onAdd();
+  }
+});
+
+// 이벤트 위임
+contents.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.content[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
